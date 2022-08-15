3177762b9acdd377a8b0389709f92d3b9bf831ca
FranciscoPizarroBianchi <frpizarrob@gmail.com>
2022-08-15 17:55:57 -0400

    amen

---
+++ activate-persona.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+. /root/anaconda3/etc/profile.d/conda.sh
+conda activate stargan-v2
+python main.py --mode sample --num_domains 2 --resume_iter 100000 --w_hpf 1 \
+               --checkpoint_dir expr/checkpoints/celeba_hq \
+               --result_dir expr/results/celeba_hq \
+               --src_dir assets/representative/celeba_hq/src \
+               --ref_dir assets/representative/celeba_hq/ref
